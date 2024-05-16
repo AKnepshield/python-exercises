47c499e76dac849afb5940b554592b780ed676eb
Charles Knepshield <aknepshield@gmail.com>
2024-05-16 11:19:17 -0500

    my_family exercise

---
+++ my_family.py
@@ -0,0 +1,15 @@
+my_family = {
+    "father": {"name": "Mac", "age": 79},
+    "mother": {"name": "Joan", "age": 75},
+    "sister": {"name": "Adriann", "age": 45},
+}
+
+print(
+    f'{my_family["father"]["name"]} is my father and is {my_family["father"]["age"]} years old'
+)
+print(
+    f'{my_family["mother"]["name"]} is my mother and is {my_family["mother"]["age"]} years old'
+)
+print(
+    f'{my_family["sister"]["name"]} is my sister and is {my_family["sister"]["age"]} years old'
+)
